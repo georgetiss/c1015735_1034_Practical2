cdaf25dda01fe48c8623de1d76eccdc602abad14
c1015735 <c1015735@newcastle.ac.uk>
2021-11-02 10:58:57 +0000

    started on file handling

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+import csv
 
 class Contact:
 
@@ -10,6 +10,13 @@ class Contact:
             self.number = number
             self.address = address
             self. bday = bday
+
+            data = [self.name, self.number, self.address, self.bday]
+
+        with open('contact_data.csv', 'a+', newline='') as savecsv:
+            #code adapted from https://www.pythontutorial.net/python-basics/python-write-csv-file/#:~:text=%20To%20write%20data%20into%20a%20CSV%20file%2C,you%20complete%20writing%20data%20to%20it.%20More%20
+            data_writer = csv.writer(savecsv)
+            data_writer.writerow(data)
 
     def __str__(self):
         return "Contact({},{},{},{})".format(self.name, self.number, self.address, self.bday)
@@ -25,3 +32,7 @@ class Contact:
 
     def search(self):
         pass
+
+
+contact1 = Contact("George Ti", 134141414, "58 Street", "1st OCt 2002")
+print(contact1.__str__())
